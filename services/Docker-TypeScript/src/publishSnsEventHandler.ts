4bd31e5d72f4e83d4e232d8799a1597d1cc5d3a2
RomanBorysenok <Roman_Borysenok@epam.com>
2017-09-06 15:54:22 +0300

    added tpescript comp version

---
+++ services/Docker-TypeScript/src/publishSnsEventHandler.ts
@@ -0,0 +1,19 @@
+import * as AWS from 'aws-sdk';
+
+export function publishSnsEventHandler(event, context) {
+    try {
+        let eventText = JSON.stringify(event, null, 2);
+        console.log("Received dgdfshsfh:", eventText);
+        
+        let sns = new AWS.SNS();
+        let params : AWS.SNS.Types.PublishInput = {
+            Message: event.pathParameters.event, 
+            Subject: event.body,
+            TopicArn: process.env.SNS_TOPIC_ARN
+        };
+        sns.publish(params, context.done);
+    } catch(e) {
+        console.log.apply(console, e);
+    }
+    
+}
